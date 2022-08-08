e33f07718edbce63d83d6a20e1a0b70eb12dadbe
Lara Vieira <contato@laravieira.me>
2022-08-08 19:41:34 -0300

    Followed class 4 of module 3.

---
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo3/Aula03/assets/js/script.js
@@ -0,0 +1,13 @@
+let a = 1, b = 2;
+
+console.log("a >= b é ", a >= b);
+console.log("a <= b é ", a <= b);
+console.log("a != b é ", a != b);
+console.log("a == b é ", a == b);
+
+// just an extra stuff
+let c = "1";
+console.log("a != c é ", a != c);
+console.log("a == c é ", a == c);
+console.log("a !== c é ", a !== c);
+console.log("a === c é ", a === c);
