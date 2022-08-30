10bfdf180b64fa4fb5dcd80d81ffaea83e0c899a
Lara Vieira <contato@laravieira.me>
2022-08-30 19:30:14 -0300

    Followed class 8 of module 5.

--- DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Component/Nav/Nav.component.js
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Component/Nav/Nav.component.js
@@ -1,16 +1,18 @@
-function NavComponent() {
-  return (
+function NavComponent({activeTab, onTabChange}) {
+    const itemClass = (tabName) => `App-nav-item ${ activeTab === tabName ? 'selected': '' }`;
+
+    return (
       <nav className="App-nav">
         <ul>
-          <li className="App-nav-item">
-            <button>Itens</button>
+          <li className={ itemClass('items') }>
+            <button onClick={ () => onTabChange('items') }>Itens</button>
           </li>
-          <li className="App-nav-item">
-            <button>Cart</button>
+          <li className={ itemClass('cart') }>
+            <button onClick={ () => onTabChange('cart') }>Cart</button>
           </li>
         </ul>
       </nav>
-  );
+    );
 }
 
 export default NavComponent;
