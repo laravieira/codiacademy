d94afed953d00222842dee89f18f058f5d810902
Lara Vieira <contato@laravieira.me>
2022-08-30 19:13:59 -0300

    Followed class 7 of module 5.

--- DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Component/App/App.component.js
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Component/App/App.component.js
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import Nav from 'Component/Nav';
 import './App.style.css';
 
 const App = () => {
-  return (
+    const [activeTab, setActiveTab] = useState('items');
+
+    return (
     <div className='App'>
-      <Nav/>
+      <Nav activeTab={activeTab} onTabChange={setActiveTab}/>
       <main className='App-content'>
-        <span>Empty</span>
+          <Content tab={activeTab}/>
       </main>
     </div>
-  );
+    );
+}
+
+const Content = ({tab}) => {
+  switch (tab) {
+      case 'items':
+          return <span>the items</span>;
+      case 'cart':
+          return <span>the cart</span>;
+  }
 }
 
 export default App;
