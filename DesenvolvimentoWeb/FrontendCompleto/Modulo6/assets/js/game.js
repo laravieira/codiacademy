b4b91a3e6c0df877ab4b1777b4300761b0794684
Lara Vieira <contato@laravieira.me>
2022-08-31 15:55:31 -0300

    Followed class 3 of module 6.

--- DesenvolvimentoWeb/FrontendCompleto/Modulo6/assets/js/game.js
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo6/assets/js/game.js
@@ -1,22 +1,34 @@
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, .1, 1000);
-renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, .1, 1000);
+const renderer = new THREE.WebGLRenderer();
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00FF00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const loader = new THREE.GLTFLoader();
+loader.load('assets/model/doll/scene.gltf', function (gltf) {
+    scene.add(gltf.scene);
+    gltf.scene.scale.set(.4, .4, .4);
+    gltf.scene.position.set(0, -1, 0);
+});
+
+const light = new THREE.AmbientLight(0xDDDDDD);
+scene.add(light);
+
+renderer.setClearColor(0x8601AF, 1);
 
 camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 
+function onWindowResize() {
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
 window.onload = animate;
+window.onresize = onWindowResize;
