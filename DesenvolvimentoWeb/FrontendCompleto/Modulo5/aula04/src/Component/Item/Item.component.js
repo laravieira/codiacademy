80c8259d9b627d7fbda2a66f1440da5159c53e3f
Lara Vieira <contato@laravieira.me>
2022-08-30 21:03:41 -0300

    Followed class 10 of module 5.

--- DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Component/Item/Item.component.js
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Component/Item/Item.component.js
@@ -1,5 +1,29 @@
-function ItemComponent() {
-    return <div/>;
+import './Item.style.css';
+import PropTypes from "prop-types";
+
+function Item({ item, onAddToCart }) {
+    return (
+        <div className="Item">
+            <div className="Item-left">
+                <div className="Item-image"><img src={ item.picture } alt={ item.name }/></div>
+            </div>
+
+            <div className="Item-middle">
+                <div className="Item-title">{ item.name }</div>
+                <div className="Item-description">{ item.description }</div>
+            </div>
+
+            <div className="Item-right">
+                <div className="Item-price">R$: { item.price }</div>
+                <div><button className="Item-addToCart" onClick={ onAddToCart }>Adicionar ao carrinho</button></div>
+            </div>
+        </div>
+    );
 }
 
-export default ItemComponent;
+Item.propTypes = {
+    item: PropTypes.object.isRequired,
+    onAddToCart: PropTypes.func.isRequired
+};
+
+export default Item;
