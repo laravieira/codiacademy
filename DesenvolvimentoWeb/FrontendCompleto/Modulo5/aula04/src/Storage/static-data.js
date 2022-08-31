448cd51ecc2c29be30f7a3bbfae17bc5ebb2a2e6
Lara Vieira <contato@laravieira.me>
2022-08-30 21:09:36 -0300

    Followed class 10 of module 5.

--- DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Storage/static-data.js
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula04/src/Storage/static-data.js
@@ -2,31 +2,31 @@ let items = [
     {
         id: 0,
         name: "Notebook Dell Inspiron",
-        image: "https://source.unplash.com/notebook",
+        picture: "https://source.unsplash.com/random?notebook",
         description: "Excelente para programação",
         price: 3029.36
     },{
         id: 1,
         name: "Celular K12+",
-        image: "https://source.unplash.com/smartphone",
+        picture: "https://source.unsplash.com/random?smartphone",
         description: "Em promoção",
         price: 926.36
     },{
         id: 2,
         name: "Mousepad Gamer",
-        image: "https://source.unplash.com/mousepad",
+        picture: "https://source.unsplash.com/random?mousepad",
         description: "O mais confortável do mercado",
         price: 129.18
     },{
         id: 3,
         name: "Monitor Samsung",
-        image: "https://source.unplash.com/monitor",
+        picture: "https://source.unsplash.com/random?monitor",
         description: "A melhor imagem OLED",
         price: 1099.90
     },{
         id: 4,
         name: "Teclado ReDragon",
-        image: "https://source.unplash.com/keyboard",
+        picture: "https://source.unsplash.com/random?keyboard",
         description: "Com retro illumination",
         price: 379.00
     },
