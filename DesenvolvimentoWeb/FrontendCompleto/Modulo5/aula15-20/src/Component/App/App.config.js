77974584ee9e67105a6e003425edbe63bcc8cbc6
Lara Vieira <contato@laravieira.me>
2022-08-31 04:09:13 -0300

    Followed class 19 of module 5.

--- DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula15-20/src/Component/App/App.config.js
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula15-20/src/Component/App/App.config.js
@@ -5,3 +5,5 @@ export const BUTTONS_LAYOUT_VALUES = [
     [1, 2, 3, "+"],
     [0, ".", "="],
 ];
+export const INPUT_LOCALE_PATTERN = '/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g';
+export const OUTPUT_LOCALE_PATTERN = '/\\s/g';
--- DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula15-20/src/Component/App/App.container.js
+++ DesenvolvimentoWeb/FrontendCompleto/Modulo5/aula15-20/src/Component/App/App.container.js
@@ -1,58 +1,73 @@
 import { useState } from 'react';
 import AppComponent from './App.component';
+import {
+    INPUT_LOCALE_PATTERN,
+    OUTPUT_LOCALE_PATTERN
+} from './App.config';
 
 function App() {
     const [calc, setCalc] = useState({
         sign: '',
         num: 0,
-        res: 0,
+        res: 0
     });
 
     const math = (a, b, sign) => {
         return sign === '+' ? a + b : sign === '-' ? a - b : sign === 'X' ? a * b : a / b;
     };
 
+    const toLocaleString = (num) => String(num).replace(INPUT_LOCALE_PATTERN, '$1 ');
+
+    const removeSpaces = (num) => num.toString().replace(OUTPUT_LOCALE_PATTERN, '');
+
     const onResetClickHandler = () => {
         setCalc({
             ...calc,
             sign: '',
             num: 0,
-            res: 0,
+            res: 0
         });
     };
 
     const onInvertClickHandler = () => {
+        const num = removeSpaces(calc.num);
+        const res = removeSpaces(calc.res);
+
         setCalc({
             ...calc,
-            num: calc.num ? calc.num * -1 : 0,
-            res: calc.res ? calc.res * -1 : 0,
-            sign: '',
+            num: calc.num ? toLocaleString(num * -1) : 0,
+            res: calc.res ? toLocaleString(res * -1) : 0,
+            sign: ''
         });
     };
 
     const onPercentClickHandler = () => {
-        const num = calc.num ? parseFloat(calc.num) : 0;
-        const res = calc.res ? parseFloat(calc.res) : 0;
+        const num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
+        const res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
 
         setCalc({
             ...calc,
             num: num / Math.pow(100, 1),
             res: res / Math.pow(100, 1),
-            sign: '',
+            sign: ''
         });
     };
 
     const onEqualsClickHandler = () => {
         const canDivide = calc.num === '0' && calc.sign === '/';
+        const num = removeSpaces(calc.num);
+        const res = removeSpaces(calc.res);
 
         if(!calc.sign || !calc.num)
             return;
 
+        const result = toLocaleString(math(Number(res), Number(num), calc.sign));
+
         setCalc({
             ...calc,
-            res: canDivide ? 'Can\'t divide by 0' : math(Number(calc.res), Number(calc.num), calc.sign),
-            sign: '',
             num: 0,
+            res: canDivide ? 'Can\'t divide by 0' : result,
+            sign: ''
         });
     };
 
@@ -75,25 +90,27 @@ function App() {
             ...calc,
             sign: value,
             res: !calc.res && calc.num ? calc.num : calc.res,
-            num: 0,
+            num: 0
         });
     };
 
     const onNumClickHandler = (event) => {
         event.preventDefault();
         const value = event.target.innerHTML;
-        let number = calc.num + value;
+        const num = removeSpaces(calc.num);
 
-        if(calc.num === 0 && value === '0')
+        let number = toLocaleString(num + value);
+
+        if(num === 0 && value === '0')
             number = '0';
-        else if(calc.num % 1 === 0)
-            number = Number(calc.num + value);
+        else if(num % 1 === 0)
+            number = toLocaleString(Number(num + value));
 
-        if(calc.num.length < 16)
+        if(num.length < 16)
             setCalc({
                 ...calc,
                 num: number,
-                res: !calc.sign ? 0 : calc.res,
+                res: !calc.sign ? 0 : calc.res
             });
     };
 
