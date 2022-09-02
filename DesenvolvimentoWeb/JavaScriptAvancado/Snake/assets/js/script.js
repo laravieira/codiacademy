28ecb0d31acd907f19bbceab009f7eabd15e644c
Lara Vieira <contato@laravieira.me>
2022-09-01 23:35:11 -0300

    Followed class 1 of snake game of advanced javascript.

--- DesenvolvimentoWeb/JavaScriptAvancado/Snake/assets/js/script.js
+++ DesenvolvimentoWeb/JavaScriptAvancado/Snake/assets/js/script.js
@@ -1,9 +1,11 @@
 class Game {
     static GRID = 20;
     static SIZE = 3;
+    static COLOR_BLUE = '#2980B9';
+    static COLOR_GREEN = '#00F102';
 
     snake = [];
-    position = { x: 10, y: 20 };
+    position = { x: 10, y: 10 };
     velocity = { x: 0, y: 0 };
     food = { x: 15, y: 15 };
 
@@ -11,10 +13,39 @@ class Game {
         this.canvas = document.getElementById('game');
         this.context = this.canvas.getContext('2d');
     }
+
+    renderGround() {
+        this.context.fillStyle = Game.COLOR_BLUE;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
+    renderSnake() {
+        this.context.fillStyle = Game.COLOR_GREEN;
+        for(let i = 0; i < this.snake.length; i++) {
+            this.context.fillRect(
+                this.snake[i].x * Game.GRID,
+                this.snake[i].y * Game.GRID,
+                Game.GRID-1,
+                Game.GRID-1
+            );
+        }
+    }
+
+    update() {
+        this.renderGround();
+        this.renderSnake();
+
+        this.snake[0] = ({
+            x: this.position.x,
+            y: this.position.y
+        });
+    }
 }
 
 function init() {
     const game = new Game();
+    setInterval(game.update.bind(game), 100);
+    window.game = game;
 }
 
 window.onload = init;
