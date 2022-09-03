c22ce274d6eb69210867752eafa1f04d84cec47b
Lara Vieira <contato@laravieira.me>
2022-09-03 16:17:15 -0300

    Followed class 2 of codi memory game of advanced javascript.

--- DesenvolvimentoWeb/JavaScriptAvancado/CodiMemory/assets/js/script.js
+++ DesenvolvimentoWeb/JavaScriptAvancado/CodiMemory/assets/js/script.js
@@ -1,21 +1,73 @@
 class Game {
+
+    cards = [];
+
     constructor() {
+        this.board = document.querySelector('.grid');
         this.loadAssets();
+        this.loadBoard();
     }
 
     loadAssets() {
-        this.asset.path = {
-            beehive: 'assets/img/beehive.svg',
-            beetle: 'assets/img/beetle.svg',
-            bird: 'assets/img/bird.svg',
-            horse: 'assets/img/horse.svg',
-            koala: 'assets/img/koala.svg',
-            panda: 'assets/img/panda.svg',
-            pelican: 'assets/img/pelican.svg',
-            penguin: 'assets/img/penguin.svg',
-            tiger: 'assets/img/tiger.svg',
-            walrus: 'assets/img/walrus.svg',
+        this.asset = {
+            paths: {
+                //beehive: 'assets/img/beehive.svg',
+                beetle: 'assets/img/beetle.svg',
+                bird: 'assets/img/bird.svg',
+                horse: 'assets/img/horse.svg',
+                koala: 'assets/img/koala.svg',
+                panda: 'assets/img/panda.svg',
+                pelican: 'assets/img/pelican.svg',
+                penguin: 'assets/img/penguin.svg',
+                tiger: 'assets/img/tiger.svg',
+                //tree: 'assets/img/tree.svg',
+                walrus: 'assets/img/walrus.svg'
+            }
         };
+    }
+
+    loadBoard() {
+        const template = document.querySelector('.template');
+        for(const key of Object.keys(this.asset.paths)) {
+            const card = template.cloneNode(true);
+            card.className = 'card';
+            card.setAttribute('key', key);
+            card.querySelector('img').setAttribute('src', this.asset.paths[key]);
+            const copy = card.cloneNode(true);
+            card.addEventListener('click', this.onCardClick.bind(this));
+            copy.addEventListener('click', this.onCardClick.bind(this));
+            this.cards.push(card, copy);
+        }
+        this.shuffleArray(this.cards);
+        this.cards.forEach((card) => this.board.appendChild(card));
+    }
+
+    onCardClick(event) {
+        this.toggleCard(event.currentTarget);
+    }
+
+    toggleCard(card) {
+        if(card.className === 'card')
+            this.showCard(card);
+        else
+            this.hideCard(card);
+    }
+
+    showCard(card) {
+        card.className = 'card flip';
+    }
+
+    hideCard(card) {
+        card.className = 'card';
+    }
+
+    shuffleArray(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
     }
 }
 
