2c78df1a492d6069193d1ca94eb40047eff02eb8
Lara Vieira <contato@laravieira.me>
2022-09-03 03:53:43 -0300

    Followed class 2 of flappy bird game of advanced javascript.

--- DesenvolvimentoWeb/JavaScriptAvancado/FlappyBird/assets/js/script.js
+++ DesenvolvimentoWeb/JavaScriptAvancado/FlappyBird/assets/js/script.js
@@ -14,18 +14,110 @@ class Game {
             x: 33,
             y: 200
         },
-        angle: 0
+        angle: .3
     }
     score = 0;
+    pipes = [];
 
     constructor() {
         this.canvas = document.getElementById('game');
         this.context = this.canvas.getContext('2d');
 
         this.loadAssets();
+        this.update();
     }
 
-    update() {}
+    renderCity() {
+        if(this.asset.city === undefined) {
+            this.asset.city = this.asset.city_day;
+            const hour = new Date().getHours();
+
+            if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+                this.asset.city = this.asset.city_night;
+            if(hour < 6 || hour > 17)
+                this.asset.city = this.asset.city_night;
+        }
+
+        this.context.drawImage(
+            this.asset.city,
+            0,
+            0,
+            this.canvas.width,
+            this.canvas.height);
+    }
+
+    renderBase() {
+        this.context.drawImage(
+            this.asset.base,
+            0,
+            this.canvas.height - this.asset.base.height,
+            this.canvas.width,
+            this.asset.base.height);
+    }
+
+    renderBird() {
+        let asset = this.asset.bird_mid_flap;
+        if(this.bird.mode === Game.BIRD_FLAP_UP)
+            asset = this.asset.bird_up_flap;
+        if(this.bird.mode === Game.BIRD_FLAP_DOWN)
+            asset = this.asset.bird_down_flap;
+
+        this.context.save();
+        this.context.translate(
+            this.bird.position.x + asset.width / 2,
+            this.bird.position.y + asset.height / 2
+        );
+        this.context.rotate(-this.bird.angle);
+        this.context.drawImage(
+            asset,
+            -asset.width / 2,
+            -asset.height / 2,
+            asset.width,
+            asset.height
+        );
+
+        this.context.restore();
+    }
+
+    flap() {
+        this.bird.position.y -= 26;
+        this.bird.angle = .3;
+        this.asset.bird_flap.cloneNode().play();
+        this.bird.mode = Game.BIRD_FLAP_DOWN;
+        setTimeout(() => this.bird.mode = Game.BIRD_FLAP_UP, 100);
+        setTimeout(() => this.bird.mode = Game.BIRD_FALLING, 210);
+    }
+
+    onKeydown(event) {
+        if(this.asset.isLoaded < this.asset.length)
+            return;
+
+        switch (event.key) {
+            case 'w':
+            case ' ':
+            case 'ArrowUp':
+                this.flap();
+                break;
+            default:
+                break;
+        }
+    }
+
+    updateBird() {
+        this.bird.position.y += Game.GRAVITY;
+        this.bird.angle -= Game.GRAVITY / 150;
+    }
+
+    update() {
+        if(this.asset.isLoaded === this.asset.length) {
+            this.updateBird();
+            this.renderCity();
+            this.renderBase();
+            this.renderBird();
+        }
+
+        requestAnimationFrame(this.update.bind(this));
+    }
 
     loadAssets() {
         this.asset = {
@@ -40,11 +132,13 @@ class Game {
                 bird_flap: 'assets/audio/fly.mp3',
                 score: 'assets/audio/score.mp3'
             },
+            length: 0,
             isLoaded: 0
         };
 
         for(const key of Object.keys(this.asset.paths)) {
             let asset = null;
+            this.asset.length++;
             if(this.asset.paths[key].endsWith('png')) {
                 asset = new Image();
                 asset.onload = () => this.asset.isLoaded++;
@@ -63,5 +157,6 @@ class Game {
 function init() {
     const game = new Game();
     window.game = game;
+    window.onkeydown = game.onKeydown.bind(game);
 }
 window.onload = init;
