f279a5f0ba9873323d93129038d3cb7128bf42e5
Lara Vieira <contato@laravieira.me>
2022-09-04 19:07:30 -0300

    Followed class 2 of Dino game of advanced javascript.

--- DesenvolvimentoWeb/JavaScriptAvancado/Dino/assets/js/script.js
+++ DesenvolvimentoWeb/JavaScriptAvancado/Dino/assets/js/script.js
@@ -1,8 +1,6 @@
 class Game {
     jumping = false;
-    gravity = .9;
     gameOver = false;
-    dinoHeight = 0;
 
     constructor() {
         this.desert = document.getElementById('desert');
@@ -10,9 +8,40 @@ class Game {
         this.grid = document.querySelector('.grid');
         this.dino = document.querySelector('.dino');
     }
+
+    jump() {
+        if(this.jumping)
+            return;
+        requestAnimationFrame(this.updateJump.bind(this));
+    }
+
+    onKeydown(event) {
+        if(event.key === ' ')
+            this.jump();
+    }
+
+    updateJump(timestamp) {
+        if(!this.jumping)
+            this.jumping = timestamp;
+        const progress = timestamp - this.jumping;
+
+        // Animation code
+        // Yf = Yo + VoT - GT^2/2
+        const dinoHeight = 1.4 * progress - .01 * progress * progress / 2;
+
+        if(dinoHeight >= 0) {
+            this.dino.style.bottom = Math.floor(dinoHeight) + 'px';
+            requestAnimationFrame(this.updateJump.bind(this));
+        }else {
+            this.dino.style.bottom = '0';
+            this.jumping = false;
+        }
+    }
 }
+
 function init() {
     const game = new Game();
     window.game = game;
+    window.onkeydown = game.onKeydown.bind(game);
 }
 window.addEventListener('DOMContentLoaded', init);
